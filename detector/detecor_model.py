09d1f321a4a8adb6a8cae1f383d0a5f6464cfdc4
肖尚华 <hallazie@gmail.com>
2020-03-17 23:54:49 +0800

    --amend

--- detector/detecor_model.py
+++ detector/detecor_model.py
@@ -1,3 +1,67 @@
 # coding:utf-8
 #
 # model by torch
+
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+	def __init__(self, cin, cout, ksize):
+		self.conv = nn.Sequential(
+			nn.Conv2d(cin, cout, kernel_size=ksize)
+			nn.BatchNorm(cout)
+			nn.ReLU(inplace=True)
+		)
+
+	def forward(self, x):
+		x = self.conv(x)
+		return x
+
+class PoolBlock(nn.Module):
+	def __init__(self, cin, cout, ksize):
+		self.pool = nn.Sequential(
+			nn.MaxPool2d(2, stride=2)
+		)
+
+	def forward(self, x):
+		x = self.pool(x)
+		return x	
+
+class YoloFace(nn.Module):
+	def __init__(self):
+		super(YoloFace, self).__init__()
+		pass
+
+	def forward(self, x):
+		return x
+
+class Model(nn.Module):
+	def __init__(self):
+		super(Model, self).__init__()
+
+		self.backbone = nn.Sequential(
+			ConvBlock(3, 64, 3),
+			ConvBlock(64, 64, 3),
+			PoolBlock(),
+			ConvBlock(64, 128, 3),
+			ConvBlock(128, 128, 3),
+			PoolBlock(),
+			ConvBlock(128, 256, 3),
+			ConvBlock(256, 256, 3),
+			PoolBlock(),
+			ConvBlock(256, 512, 3)
+			ConvBlock(512, 512, 3)
+			PoolBlock(),
+			ConvBlock(512, 512, 3)
+			ConvBlock(512, 512, 3)
+			PoolBlock(),
+			ConvBlock(512, 5, 1)
+			YoloFace()
+			)
+
+	def nms(self, x):
+		pass
+
+	def forward(self, x):
+		pass
+
--- fraction.py
+++ fraction.py
@@ -3,11 +3,12 @@
 from PIL import Image
 
 import numpy as np
+import torch
 
-img = np.array(Image.open('data/th.jpg'))
-print(img.shape)
+# img = np.array(Image.open('data/th.jpg'))
+# print(img.shape)
 
-exp = np.array([[[img[(x-1)//2, (h-1)//2, c] if x%2==0 else 0 for x in range(img.shape[-3]*2+1)] if h%2==0 else [0 for x in range(img.shape[-3]*2+1)] for h in range(img.shape[-2]*2+1)] for c in range(img.shape[-1])]).transpose()
-print(exp.shape)
+# exp = np.array([[[img[(x-1)//2, (h-1)//2, c] if x%2==0 else 0 for x in range(img.shape[-3]*2+1)] if h%2==0 else [0 for x in range(img.shape[-3]*2+1)] for h in range(img.shape[-2]*2+1)] for c in range(img.shape[-1])]).transpose()
+# print(exp.shape)
 
-Image.fromarray(exp.astype('uint8')).save('data/th-exp.jpg')
+# Image.fromarray(exp.astype('uint8')).save('data/th-exp.jpg')
